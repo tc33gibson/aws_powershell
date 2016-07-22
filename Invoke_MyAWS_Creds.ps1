185c8c62c030ffe598ebf3d6493e248eaeb386c1
tc33gibson <tc33gibson@gmail.com>
2016-07-22 14:33:23 -0400

    Added comments to top

--- Invoke_MyAWS_Creds.ps1
+++ Invoke_MyAWS_Creds.ps1
@@ -1,4 +1,11 @@
-﻿$AWSAccessKey = "Enter Key Here" #Update this - Add Access Key
+﻿# The following script is designed to be called from other scripts as means to provide 
+# a simple method to authentication to AWS and provide Windows credintials to an internet proxy
+
+<#Place this line at the top of your script
+& ((Split-Path $MyInvocation.InvocationName) + "\Invoke_MyAWS_Creds")  #Update the "\" to the file path if not in the same directory
+#>
+
+$AWSAccessKey = "Enter Key Here" #Update this - Add Access Key
 $AWSSecretKey = "Enter Key Here" #Update this - Add Secret Key
 $AWSProfile = "Provide a local profile" #Update this - Set for nonprod or prod later
 $AWSRegion = "us-east-1" #Set default region - optional
